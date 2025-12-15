e377538f3c95f038b021656b2fccf4322182f9d3
shinokun <hypatia.tile02021@gmail.com>
2025-12-15 23:25:06 +0900

    Dispatch functino for MyLib module for the modular structure

--- tiny-repl/app/Main.hs
+++ tiny-repl/app/Main.hs
@@ -1,21 +1,8 @@
 module Main where
 
+import MyLib (mainLoop)
+
 main :: IO ()
 main = do
   putStrLn "Hello, This is Tiny REPL!"
   mainLoop
-
-mainLoop :: IO ()
-mainLoop = do
-  putStrLn "Input something (or type 'exit' to quit):"
-  input <- getLine
-  case interpret input of
-    Just output -> do
-      putStrLn output
-      mainLoop
-    Nothing -> putStrLn "Goodbye!"
-
-interpret :: String -> Maybe String
-interpret input = case input of
-  "exit" -> Nothing
-  _ -> Just $ "Interpreted: " ++ input
--- tiny-repl/src/MyLib.hs
+++ tiny-repl/src/MyLib.hs
@@ -1,4 +1,16 @@
-module MyLib (someFunc) where
+module MyLib (mainLoop) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+mainLoop :: IO ()
+mainLoop = do
+  putStrLn "Input something (or type 'exit' to quit):"
+  input <- getLine
+  case interpret input of
+    Just output -> do
+      putStrLn output
+      mainLoop
+    Nothing -> putStrLn "Goodbye!"
+
+interpret :: String -> Maybe String
+interpret input = case input of
+  "exit" -> Nothing
+  _ -> Just $ "Interpreted: " ++ input
