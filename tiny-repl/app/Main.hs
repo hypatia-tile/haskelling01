5a2251bb4016acf3e6773f18fded49fdce566a2c
shinokun <hypatia.tile02021@gmail.com>
2025-12-15 23:06:35 +0900

    Add mainLoop function to scale my tidy app

--- tiny-repl/app/Main.hs
+++ tiny-repl/app/Main.hs
@@ -3,6 +3,19 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "Hello, This is Tiny REPL!"
-  putStrLn "Input something:"
+  mainLoop
+
+mainLoop :: IO ()
+mainLoop = do
+  putStrLn "Input something (or type 'exit' to quit):"
   input <- getLine
-  putStrLn $ "Your input was: " ++ input
+  case interpret input of
+    Just output -> do
+      putStrLn output
+      mainLoop
+    Nothing -> putStrLn "Goodbye!"
+
+interpret :: String -> Maybe String
+interpret input = case input of
+  "exit" -> Nothing
+  _ -> Just $ "Interpreted: " ++ input
