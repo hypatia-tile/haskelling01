68b11371458a3327c08b51466d0eff59cd44cab8
shinokun <hypatia.tile02021@gmail.com>
2025-12-20 12:07:48 +0900

    Use Either Monad instead Maybe for expressive error handling

--- tiny-repl/src/MyLib.hs
+++ tiny-repl/src/MyLib.hs
@@ -1,30 +1,35 @@
 module MyLib (mainLoop) where
 
-import Control.Monad.Except
-import Control.Monad.State
+-- import Control.Monad.Except
+-- import Control.Monad.State
 
 mainLoop :: IO ()
 mainLoop = do
   putStrLn "Input something (or type 'exit' to quit):"
   input <- getLine
   case interpret input of
-    Just output -> do
-      putStrLn output
-      mainLoop
-    Nothing -> putStrLn "Goodbye!"
+    Left err -> do
+      putStrLn $ "Error: " ++ show err
+    Right appState -> do
+      putStrLn $ output appState
+      if exit appState
+        then putStrLn "Exiting..."
+        else mainLoop
 
-interpret :: String -> Maybe String
+-- TODO: Implement a real interpreter
+interpret :: String -> Either AppError AppState
 interpret input = case input of
-  "exit" -> Nothing
-  _ -> Just $ "Interpreted: " ++ input
+  "exit" -> Right $ AppState True "Goodbye!"
+  _ -> Right $ AppState False ("Interpreted: " ++ input)
 
+-- TODO: Define a proper AppState and AppError
 data AppState = AppState
-  {}
+  { exit :: Bool
+  , output :: String
+  }
 
 data AppError
   = ParseError String
   | UnknownCommand String
   | DomayinError String
   deriving (Show)
-
-type AppM = StateT AppState (ExceptT AppError IO)
