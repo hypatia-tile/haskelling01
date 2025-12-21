3d4ec08931705145f415948429a4edb163a0c614
shinokun <hypatia.tile02021@gmail.com>
2025-12-21 11:51:54 +0900

    Add example for stateful sum computation

--- plain/mtl-example/StateTExample01.hs
+++ plain/mtl-example/StateTExample01.hs
@@ -1,4 +1,7 @@
+import Control.Applicative qualified as Int
+import Control.Monad (forM_)
 import Control.Monad.State.Strict
+import Control.Monad.Trans.Class (lift)
 
 -- StateT Int IO String represents:
 --   Int -> IO (String, Int)
@@ -41,8 +44,39 @@ myState = gets myFunc
 evalMyState :: Int -> IO String
 evalMyState = evalStateT myState
 
-main :: IO ()
-main = do
+main01 :: IO ()
+main01 = do
   -- Run the stateful computation with initial state 42
   result <- evalMyState 42
   putStrLn result
+
+{- | Stateful computation has a state of type Int.
+    If runStateT is applied, the inner function has type:
+      Int -> IO ((), Int)
+    When the state is provided, it returns an IO action that produces a tuple of:
+      - a value of type ()
+      - a new state of type Int
+-}
+statefulCalc :: Int -> StateT Int IO ()
+statefulCalc i = do
+  modify (+ i)
+  v <- get
+  lift $ putStrLn $ "Added " ++ show i ++ ", current sum: " ++ show v
+
+sum' :: [Int] -> IO Int
+sum' xs = (`execStateT` 0) $ do
+  forM_ xs statefulCalc
+
+-- sum' xs = (`execState` 0) $ do
+--   forM_ xs $ statefulSum
+
+main02 :: IO ()
+main02 = do
+  print =<< sum' [1 .. 10]
+
+-- let numbers = [1 .. 10]
+-- print $ sum' numbers
+
+main :: IO ()
+main = do
+  main01
