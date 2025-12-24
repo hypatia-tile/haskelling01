abb6b9a3b81821f481ae806610c4960f68d09362
shinokun <hypatia.tile02021@gmail.com>
2025-12-24 20:18:12 +0900

    Reimplement StateT with instantiation for type classes Functor, Applicative and Monad.

---
+++ plain/mtl-example/StateTExample06.hs
@@ -0,0 +1,45 @@
+-- In this example, I will rewrite the StateT's instantiations for important type classes.
+import Control.Monad.Trans.State.Lazy hiding (StateT (..))
+
+-- | Define the StateT type
+newtype StateT s m a = StateT {runStateT :: s -> m (a, s)}
+
+instance (Functor m) => Functor (StateT s m) where
+  fmap f g = StateT $ \x -> fmap (\ ~(a, s) -> (f a, s)) (runStateT g x)
+
+instance (Functor m, Monad m) => Applicative (StateT s m) where
+  pure a = StateT $ \s -> pure (a, s)
+  StateT mf <*> StateT mx = StateT $ \s -> do
+    ~(f, s') <- mf s
+    ~(x, s'') <- mx s'
+    return (f x, s'')
+
+instance (Monad m) => Monad (StateT s m) where
+  g >>= f = StateT $ \s -> do
+    (a, newState) <- runStateT g s
+    runStateT (f a) newState
+
+stateCompute :: StateT Int IO String
+stateCompute = do
+  StateT $ \s -> do
+    putStrLn $ "CurrentState: " ++ show s
+    return ("Value is " ++ show s, s + 1)
+
+main :: IO ()
+main = do
+  let
+    computation :: StateT Int IO String
+    computation = do
+      stateCompute
+      stateCompute
+  result <- runStateT computation 1
+  putStrLn $ "Final Result: " ++ show result
+
+main01 :: IO ()
+main01 = do
+  let
+    computation = do
+      modify (+ 1)
+      modify (+ 1)
+  result <- execStateT computation 1
+  putStrLn $ "Final State: " ++ show result
