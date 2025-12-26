d814fd7535a44dab6146e7750a6633602f466e05
shinokun <hypatia.tile02021@gmail.com>
2025-12-27 07:11:42 +0900

    Use ExceptT instead of Either Monad

--- tiny-repl/src/MyLib.hs
+++ tiny-repl/src/MyLib.hs
@@ -1,5 +1,7 @@
 module MyLib (mainLoop) where
 
+import Control.Monad.Except
+
 -- import Control.Monad.Except
 -- import Control.Monad.State
 
@@ -7,26 +9,30 @@ mainLoop :: IO ()
 mainLoop = do
   putStrLn "Input something (or type 'exit' to quit):"
   input <- getLine
-  case interpret input of
+  result <- runExceptT (interpret input)
+  case result of
     Left err -> do
       putStrLn $ "Error: " ++ show err
-    Right appState -> do
-      putStrLn $ output appState
-      if exit appState
-        then putStrLn "Exiting..."
-        else mainLoop
+      mainLoop
+    Right appState -> case appState of
+      ExitAppState -> putStrLn "Exiting..."
+      RegularAppState output' -> do
+        putStrLn $ "Output: " ++ output'
+        mainLoop
 
 -- TODO: Implement a real interpreter
-interpret :: String -> Either AppError AppState
+interpret :: String -> ExceptT AppError IO AppState
 interpret input = case input of
-  "exit" -> Right $ AppState True "Goodbye!"
-  _ -> Right $ AppState False ("Interpreted: " ++ input)
+  "exit" -> ExceptT $ return (Right ExitAppState)
+  "hello" -> ExceptT $ return (Right (RegularAppState "Hello, World!"))
+  _ -> throwError (UnknownCommand input)
 
 -- TODO: Define a proper AppState and AppError
-data AppState = AppState
-  { exit :: Bool
-  , output :: String
-  }
+data AppState
+  = RegularAppState
+      { output :: String
+      }
+  | ExitAppState
 
 data AppError
   = ParseError String
