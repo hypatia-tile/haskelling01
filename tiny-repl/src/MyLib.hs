249b929c878292554dedc5372ecfc61441316dc1
shinokun <hypatia.tile02021@gmail.com>
2025-12-27 12:10:17 +0900

    Create a sub shell for arithmetic operations

--- tiny-repl/src/MyLib.hs
+++ tiny-repl/src/MyLib.hs
@@ -15,6 +15,13 @@ mainLoop initialState = do
       mainLoop initialState
     Right ((), newState) -> case newState of
       ExitAppState -> putStrLn "Exiting..."
+      ArithAppState -> do
+        putStrLn "Entering arithmetic shell..."
+        arithResult <- runExceptT $ evalStateT arithShell []
+        case arithResult of
+          Left err -> putStrLn $ "Arithmetic shell error: " ++ err
+          Right _ -> putStrLn "Exiting arithmetic shell."
+        mainLoop newState
       _ -> do
         putStrLn "History:"
         forM_ (history newState) putStrLn
@@ -31,22 +38,82 @@ interpret input = StateT $ \state' ->
 
 type AppM = StateT AppState (ExceptT AppError IO)
 
+data ArithExpr
+  = Push Int
+  | Add
+  | Sub
+  deriving (Show)
+
 data AppState
   = RegularAppState
       { history :: [String]
       , execute :: IO String
       }
+  | ArithAppState
   | ExitAppState
+
+evalArith :: [ArithExpr] -> Either String Int
+evalArith exprs = case foldr (flip go) [] exprs of
+  [Push n] -> Right n
+  xs -> Left $ "Stack did not end with a single value\n remaining stack: " ++ show xs
+ where
+  go :: [ArithExpr] -> ArithExpr -> [ArithExpr]
+  go (Push x : Add : rest) (Push y) = Push (x + y) : rest
+  go (Push x : Sub : rest) (Push y) = Push (y - x) : rest
+  go stack x = x : stack
+
+arithShell :: StateT [ArithExpr] (ExceptT String IO) ()
+arithShell = do
+  exprs' <- arithInputLoop []
+  liftIO $ putStrLn $ "Final expressions: " ++ show exprs'
+  return ()
+ where
+  arithInputLoop :: [ArithExpr] -> StateT [ArithExpr] (ExceptT String IO) Int
+  arithInputLoop exprs = do
+    liftIO $ putStrLn $ "Current expressions: " ++ show (reverse exprs)
+    liftIO $ putStrLn "Arith> "
+    input <- liftIO getLine
+    case words input of
+      ["push", nStr] -> case reads nStr of
+        [(n, "")] -> arithInputLoop (Push n : exprs)
+        _ -> do
+          liftIO $ putStrLn "Invalid number"
+          arithInputLoop exprs
+      ["add"] -> arithInputLoop (Add : exprs)
+      ["sub"] -> arithInputLoop (Sub : exprs)
+      ["eval"] -> do
+        case evalArith exprs of
+          Left err -> do
+            throwError err
+          Right val -> return val
+      _ -> do
+        throwError "Unknown command"
+
+data AppCommand
+  = Exit
+  | Hello
+  | Arith
+
+parseAppCommand :: String -> Either AppError AppCommand
+parseAppCommand cmd
+  | cmd == "exit" = Right Exit
+  | cmd == "hello" = Right Hello
+  | cmd == "arith" = Right Arith
+  | otherwise = Left (UnknownCommand $ "Unknown command: " ++ cmd)
 
 appCommand :: String -> AppState -> Either AppError AppState
 appCommand cmd state'
   | null cmd = Left (ParseError "Empty command")
-  | otherwise = case cmd of
-      "exit" -> Right ExitAppState
-      "hello" -> Right $ RegularAppState (cmd : history state') (return "world")
-      "add" -> Right $ RegularAppState (cmd : history state') (return "added")
-      "sub" -> Right $ RegularAppState (cmd : history state') (return "subtracted")
-      _ -> Left (UnknownCommand cmd)
+  | otherwise = case parseAppCommand cmd of
+      Right Exit -> Right ExitAppState
+      Right Hello ->
+        Right $
+          RegularAppState
+            { history = history state' ++ [cmd]
+            , execute = return "Hello, World!"
+            }
+      Right Arith -> Right ArithAppState
+      Left err -> Left err
 
 data AppError
   = ParseError String
